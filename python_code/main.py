41fcdbd045a7e71b11fe3ccf77761fece3e0251e
nouf <noufel.ammari.42@gmail.com>
2024-03-03 11:42:01 +0100

    showcases sequential execution of python code

---
+++ python_code/main.py
@@ -0,0 +1,14 @@
+def say_hi():
+    print("Hi!")
+
+
+def add_int(left: int, right: int) -> int:
+    return left + right
+
+
+if __name__ == "__main__":
+    say_hi()
+    x = 3
+    y = 4
+    ret = add_int(x, y)
+    print(f"Result is {ret}")
